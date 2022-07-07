0b1b45d2f885857e4bf98a02c94237e3aa29eb0a
Philip Boardman <dustykeyboard@outlook.com.au>
2022-07-07 23:45:06 +1000

    Display users, articles by newest first

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
 
   # GET /users or /users.json
   def index
-    @users = User.all
+    @users = User.order('created_at DESC')
   end
 
   # GET /users/1 or /users/1.json
