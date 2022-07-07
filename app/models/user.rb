fbeb7571dcf06757a64c673cf98c797487982215
Philip Boardman <dustykeyboard@outlook.com.au>
2022-07-08 00:13:47 +1000

    Email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
   has_many :articles
+
+  validates :name, :email, presence: true, uniqueness: true
+  validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP
 end
